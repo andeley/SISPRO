492ed6bf5f6289a4a1b5cba8084d6623687a844a
Crisel Ayala <criseljazminal@ufps.edu.co>
2018-12-04 15:38:43 -0500

    Revert "reestructurando css"
    This reverts commit 4d32f72c8e2d5ee5a76f1c27230893215671d5f1.

--- application/views/director/header.php
+++ application/views/director/header.php
@@ -13,7 +13,15 @@
     <!-- Bootstrap core-->
     <link href="<?php echo base_url(); ?>assets/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_pre_style.css">
+
+    <!-- LINKS LOCALES-->
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_areas_style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_simula_style.css">
+     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_doc_style.css">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_est_style.css">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/dir_per_style.css">
+    <!-------------------->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/template/css/menu.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
