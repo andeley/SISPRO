63b0e18c7b530ba43aa9e5303c74c778b7539f6b
Crisel Ayala <criseljazminal@ufps.edu.co>
2018-12-04 15:20:18 -0500

    Revert "Merge branch 'master' of https://github.com/cryselayala98/SISPRO"
    This reverts commit 582f5ed44836cdd065be8749d297d93074f02119, reversing
    changes made to 53a2bfec9f097b1fedf0dc0f5d4bc49b266e87d5.

--- application/views/estudiante/preguntas.php
+++ application/views/estudiante/preguntas.php
@@ -1,138 +1,37 @@
+
 <div id="page_content">
-      <div class="container-fluid">
-        <?php if ($tipo == "general") {
-    ?>
-          <div id="indice_pag">
+      
+<div class="container-fluid">
+    <div id="indice_pag">          
+        <center><p>Perfil del Estudiante</p></center>
+    </div>     
+    
+    <div id="cotenido_pag">          
+      
+      jeje
 
-            <p>Gestionar > <a href="<?=base_url();?>estudiante/Preguntas">Preguntas</a></p>
-            </div>
-         <div id="cotenido_pag">
-
-<!--Ver Areas existentes para las preguntas-->
-         <div id="indice_pag">
-         <center><p>Menú de preguntas</p></center>
-         </div>
-
-         <!--contenido areas-->
-     <section id="noticias" class="info-section">
-      <div class="container">
-        <?php
-$num_filas = ceil(count($todas_las_areas) / 3);
-    $cont      = 0;
-    for ($i = 0; $i < $num_filas; $i++) {
-        ?>
-
-           <div class="row">
-            <?php for ($i = 0; $i < 3 && $cont < count($todas_las_areas); $i++, $cont++) {?>
-
-           <div class="col-md-4 mb-3 mb-md-0">
-            <div class="card py-4 h-100">
-              <div class="card-body text-center">
-                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                <h4 class="m-0"><a href="<?php echo base_url(); ?>estudiante/Preguntas/ver_preguntas_area/<?=$todas_las_areas[$cont]->id?>"><?php echo $todas_las_areas[$cont]->nombre ?></a></h4>
-              </div>
-            </div>
-          </div>
-
-          <?php }
-        ;?>
+    </div>              
 </div>
-
-        <?php }?>
-      </div>
-    </section>
-         <!--fin contenido areas-->
+    
 </div>
-<?php } else if ($tipo == "ver preguntas area") {?>
-
-    <div id="indice_pag">
-            <p>Gestionar > <a href="<?=base_url();?>estudiante/Preguntas">Preguntas</a> > <a href="<?php echo base_url(); ?>estudiante/Preguntas/ver_preguntas_area/<?=$id_a?>"><?=$nombre_area?></a></p>
-            </div>
-         <div id="cotenido_pag">
-            <?php if ($preguntas) {?>
-    <div class="table_">
-               <table class="table table-hover">
-  <thead>
-    <tr id="tit_table">
-      <th scope="col">Id</th>
-      <th scope="col">Estado</th>
-      <th scope="col">Visibilidad</th>
-      <th scope="col">Opciones</th>
-    </tr>
-  </thead>
-  <tbody>
-
-     <?php foreach ($preguntas as $pregunta): ?>
-        <tr>
-        <th scope="row"><?php echo $pregunta->id; ?></th>
-        <td><?php echo $pregunta->estado; ?></td>
-        <td><?php echo $pregunta->visibilidad; ?></td>
-        <td><center><a href="<?php echo base_url(); ?>estudiante/Preguntas/verDetalle/<?=$pregunta->id?>"><button type="button" class="btn btn-danger btn-sm">Ver detalle</button></a></center></td>
-        </tr>
-    <?php endforeach;?>
-
-  </tbody>
-</table>
-</div>
-</div>
-<?php } else {?>
-            <div id="indice_pag">
-            <center><p>No tienes Preguntas registradas</p></center>
-            </div>
-    <?php }?>
-</div>
-<?php } else if ($tipo == "ver detalle pregunta") {
-    ?>
-
-    <div id="indice_pag">
-            <p>Gestionar > <a href="<?=base_url();?>estudiante/Preguntas">Preguntas</a> > <a href="<?php echo base_url(); ?>estudiante/Preguntas/ver_preguntas_area/<?=$area_p->id?>"><?=$area_p->nombre?></a></p>
-            </div>
-         <div id="cotenido_pag">
-            <center><h3>Informacion acerca de la pregunta <?=$info_pregunta->id?></h3></center>
-<?php
-if ($enunciado != "no existe enunciado") {
-        ?>
-<p class="pru_preg"><?=$enunciado;?></p>
-<?php
-}
-    ?>
-<p><?=$info_pregunta-> descripcion?></p>
-<?php
-$i = 97;
-    foreach ($opciones_respuesta as $o) {
-        ?>
-        <p><b><?=chr($i++) . ". ";?></b><?=$o->descripcion;?></p>
-        <?php
-
-    }?>
-
-  </div>
-
-<?php } ?>
-</div>
-      </div>
-
-
-   <!----------------FOOTER---------------------->
+   
+   <!-------------------------------------->
     <footer class="small text-center text-white-50">
       <div class="container">
-        Copyright &copy; Ingenieria de software
+        Copyright &copy; Ingeniería de Software
       </div>
     </footer>
 
-   <!-- Bootstrap core JavaScript -->
-    <!--<script src="../vendor/jquery/jquery.min.js"></script>-->
-    <script src="<?php echo base_url(); ?>assets/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+   <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/template/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Plugin JavaScript -->
     <script src="<?php echo base_url(); ?>assets/template/vendor/jquery-easing/jquery.easing.min.js"></script>
-
     <!-- Custom scripts for this template -->
     <script src="<?php echo base_url(); ?>assets/template/js/grayscale.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   </body>
 
 </html>
--- application/views/estudiante/prueba.php
+++ application/views/estudiante/prueba.php
@@ -32,79 +32,17 @@
     <!-----------MENU--------------->
      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <div class="row">
-          <div class="col-md-3">
-            <a  class="navbar-brand js-scroll-trigger" href="#page-top" style="margin-top: 15px;">
-    <img src="<?php echo base_url(); ?>assets/template/img/logo_blanco.png" alt="">         
-        </a>
-          </div>
-          <div class="col-md-2" style="text-align: center;">
+        <a  class="navbar-brand js-scroll-trigger" href="#page-top">
+    <img src="<?php echo base_url(); ?>assets/template/img/logo_blanco.png" alt="">        
             
-           <div id="diferencia" class=" nav-link" >
-         
-             <!--js con tiempo de finalización-->
-             <script type="text/javascript">
-
-                      //cargar tiempo que falta para que finalice el simulacro
-                       window.onload = function(e){
-
-                          document.getElementById("pro").style.width = "<?php echo $porcentaje ?>"+"%";
-                          if(p_totales == p_respondidas) document.getElementById("envio_info").disabled  = false;
-                          else document.getElementById("envio_info").disabled  = true;
-
-                      var fin = "<?php echo $simulacro-> fecha_fin; ?>";
-                      var $clock = $('#diferencia'),
-                          eventTime = moment(fin, 'YYYY-MM-DD HH:mm:ss').unix(),
-                          currentTime = moment().unix(),
-                          diffTime = eventTime - currentTime,
-                          duration = moment.duration(diffTime * 1000, 'milliseconds'),
-                          interval = 1000;
-
-                      // if time to countdown
-                      if(diffTime > 0) {
-
-                          var $d = $('<div class="days" ></div>').appendTo($clock),
-                              $h = $('<div class="hours" ></div>').appendTo($clock),
-                              $m = $('<div class="minutes" ></div>').appendTo($clock),
-                              $s = $('<div class="seconds" ></div>').appendTo($clock);
-
-                          setInterval(function(){
-
-                              duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
-                              var d = moment.duration(duration).days(),
-                                  h = moment.duration(duration).hours(),
-                                  m = moment.duration(duration).minutes(),
-                                  s = moment.duration(duration).seconds();
-
-                             d = $.trim(d).length === 1 ? '0' + d : d;
-                             h = $.trim(d).length === 1 ? '0' + h : h;
-                             m = $.trim(m).length === 1 ? '0' + m : m;
-                             s = $.trim(s).length === 1 ? '0' + s : s;
-
-                             $d.text(h+" : "+m+" : "+s);
-                          }, interval);
-                      }
-                  };
-            </script>
-
-
-           </div>
-          </div>
-          <div class="col-md-3">
-             <center>
-              <div class="progress" style="height: 13px; align-self: center; margin-top: 35px;">
-              <div class="progress-bar" id="pro" role="progressbar" style="width: 0%; background-color: #7F072A;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo $porcentaje; ?> %</div>
-              </div>
-             </center>
-          </div>
-          <div class="col-md-4">
-          
+            
+        </a>
          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto"> 
+          <ul class="navbar-nav ml-auto">  
              <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="<?=base_url();?>estudiante/Preguntas">Preguntas</a>
             </li>
@@ -119,15 +57,10 @@
             </li>
           </ul>
         </div>
-          </div>
-        </div>
       </div>
     </nav>
 
-
 <div id="page_content">
-
-    
       
 <div class="container-fluid">
    <div id="titulo_programa">          
@@ -138,9 +71,8 @@
     </div>     
     
     <div id="cotenido_pag">          
-      <!-------------Componente Areas Preguntas------------------->
-      <form class="form-group" method="post" id="formr" action="<?php echo base_url(); ?>estudiante/Simulacros/guardar_simulacro/<?=$simulacro_id;?>?>">
-
+      <!-------------CUADRO------------------->
+      <form>
         <?php if($areas_simulacro): ?>
           
           <?php foreach ($areas_simulacro as $as):?>
@@ -158,6 +90,10 @@
                 <p><?php echo $as -> nombre; ?></p>
             </div>
             
+
+
+
+
               
               <?php $preguntas = $preguntas_area[$as -> id]; ?>
               <?php if($preguntas): ?>
@@ -182,22 +118,17 @@
                   <br>
                 <div class="pru_res">
       <?php $i = 97; //a ?>
-
       <div class="container">
       <?php foreach ($opciones [$p -> id_pregunta] as $opcion): ?>
-    
+  
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label style="color: #6E6E6E;" class="form-check-label" for="exampleCheck1">
-    <?php 
-    if(!$preguntas_respondidas) $key = -1;
-    else $key = array_search($opcion-> id, array_column($preguntas_respondidas, 'id_opcion')); ?>
-      
-      <input type="radio" name="<?=$p-> id_pregunta;?>" <?php if($key>-1){ ?> checked <?php }?>   class="form-check-input" id="exampleCheck1" value="<?=$simulacro_id."/".$p->id_pregunta."/".$opcion-> id;?>" />
-
-      <b><?php echo(chr($i++)).". "; ?></b>
       <?php echo $opcion -> descripcion; ?>
     </label><br>
   
   <?php endforeach; ?><!--for opc rta-->
+
+                    
                 </div>
                 </div>
               <?php endif; ?><!--fin if existencia opc-->
@@ -205,7 +136,7 @@
             </div>
             <?php endif; ?><!--fin if existencia pregunta-->
 
-             <?php endforeach; ?><!--for recorrer preguntas de seleccion Múltiple-->
+             <?php endforeach; ?><!--for recorrer preguntas-->
             
             </div>
             <?php endif; ?><!--if existencia de preguntas en el area-->
@@ -216,8 +147,10 @@
       </div>
       <?php  endforeach; ?>
         <?php endif; ?>
+        
 
-         <button id="envio_info" type="submit" >Finalizar</button>
+        
+         <button id="envio_info" type="submit" class="btn ">Enviar</button>
     </form> 
 
     </div> 
@@ -242,86 +175,8 @@
 
     <!-- Custom scripts for this template -->
     <script src="<?php echo base_url(); ?>assets/template/js/grayscale.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    
    
-
-<!--Guardar Automáticamebte la respuesta-->
-<script type="text/javascript">
-
-  //llenar porcentaje
-  var porc = "<?php echo $porcentaje ?>";
-
-
-
-  var respuestas_id = ["."]; //error aca
-  var p_totales = "<?php echo $num_preguntas; ?>";
-  var p_respondidas = "<?php if($preguntas_respondidas) echo count($preguntas_respondidas); else echo 0; ?>";
-
-
-
-$(document).ready( function() {
-$('input[type=radio]').change(function(e) {
-  var ruta = "<?php echo base_url(); ?>estudiante/Simulacros/anadir_rta/"+$(this).val()  ;
-  var nam = $(this).attr("name");
-  var respondidas = "<?php if($preguntas_respondidas) foreach($preguntas_respondidas as $pp) echo $pp-> id_pregunta."-"; else echo ".";?>";
-
-                $.ajax({
-                    url:  ruta,
-                    type:"POST",
-                    data: $(this).serialize(),
-                    success:function(resp){
-
-                    var ya_m = respondidas.indexOf(nam);
-                    if(respuestas_id.indexOf(nam) == -1 && ya_m == -1 ){
-                        p_respondidas++;
-                        respuestas_id.push(nam);
-                       
-                        ya_m+="nam"+"-";
-                    }
-                    var t = ((p_respondidas*100)/p_totales);
-                        document.getElementById("pro").style.width = t+"%";
-                        document.getElementById("pro").innerHTML  = parseInt(t)+"%";
-                   
-                   toastr.success('Respuesta Guardada'+resp);
-
-                   if(parseInt(t)==100){
-                      document.getElementById("envio_info").disabled  = false;
-                   }
-            }
-        });
-                e.preventDefault();
-            });
-        });
-
-        //opciones notoficacion rtas
-       toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-bottom-left",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "400",
-            "timeOut": "3000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-          }
-
-</script>
-
-<style type="text/css">
-    #toast-container > .toast-success {
-    background-image: none;
-    background-color: #900C3F;
-    color: #FFFFFF;
-}
-</style>
+   
    
   </body>
 
